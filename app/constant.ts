c75f3edae11766f32ec7d1cffd74f799555de9e1
kangkaisen <kangkaisen@apache.org>
2023-11-24 14:36:15 +0800

    Fix compile
    Signed-off-by: kangkaisen <kangkaisen@apache.org>

--- app/constant.ts
+++ app/constant.ts
@@ -102,6 +102,10 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
+    name: "gpt-3.5-turbo",
+    available: true,
+  },
+  {
     name: "gpt-3.5-turbo-1106",
     available: true,
   },
