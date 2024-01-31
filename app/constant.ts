51ab209b2119015a951efe89051a242e68c685ca
kangkaisen <kangkaisen@apache.org>
2024-01-31 16:57:23 +0800

    Change gpt4 model
    Signed-off-by: kangkaisen <kangkaisen@apache.org>

--- app/constant.ts
+++ app/constant.ts
@@ -92,13 +92,13 @@ export const SUMMARIZE_MODEL = "gpt-3.5-turbo-1106";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
   default: "2021-09",
-  "gpt-4-1106-preview": "2023-04",
+  "gpt-4-turbo-preview": "2023-04",
   "gpt-4-vision-preview": "2023-04",
 };
 
 export const DEFAULT_MODELS = [
   {
-    name: "gpt-4-1106-preview",
+    name: "gpt-4-turbo-preview",
     available: true,
   },
   // {
